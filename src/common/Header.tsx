f1b4f302f466d6c06f01fa6f0818677e9f6776db
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-06-04 10:47:29 -0400

    Animate header

--- src/common/Header.tsx
+++ src/common/Header.tsx
@@ -1,13 +1,19 @@
 import { mdiMenu } from '@mdi/js';
 import { useState } from 'react';
 import { MobileNav, DesktopNav } from './Nav';
+import { motion } from 'motion/react';
 import Icon from '@mdi/react';
 
 const Header = () => {
     const [isActive, setIsActive] = useState(false);
 
     return (
-        <div className="font-proxima text-2xl text-white sm:text-lg">
+        <motion.div
+            transition={{ ease: 'linear', duration: 0.2 }}
+            initial={{ translateY: -60, opacity: 0 }}
+            whileInView={{ translateY: 0, opacity: 1 }}
+            className="font-proxima text-2xl text-white sm:text-lg"
+        >
             <header className="relative z-100 flex items-center bg-[#111111]">
                 <div className="container mx-auto flex items-center justify-between p-5">
                     <div>Logo</div>
@@ -24,7 +30,7 @@ const Header = () => {
             </header>
 
             <MobileNav isActive={isActive} />
-        </div>
+        </motion.div>
     );
 };
 
