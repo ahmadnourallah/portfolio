45c72c3b12f298c542c09eabc0125bb22853bce1
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-06-04 10:03:00 -0400

    Set up routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RootLayout from "./pages/RootLayout";
+import PortfolioPage from "./pages/PortfolioPage";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<StrictMode></StrictMode>);
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route element={<RootLayout />}>
+					<Route index element={<PortfolioPage />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	</StrictMode>
+);
