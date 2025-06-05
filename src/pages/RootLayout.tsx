33eded14bc2d0b791d3d4d7e81c57244d6d16579
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-06-05 07:13:09 -0400

    Hide overflow for animation

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -6,7 +6,7 @@ const RootLayout = () => {
     return (
         <>
             <Header />
-            <main>
+            <main className="overflow-hidden">
                 <Outlet />
             </main>
             <Footer />
