81989138953315c27c34c5d2ecbf97d34dd975a9
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-06-05 07:13:43 -0400

    Create sections

---
+++ src/common/GreySection.tsx
@@ -0,0 +1,19 @@
+import type { ReactNode } from 'react';
+
+const GreySection = ({
+    children,
+    className
+}: {
+    children: ReactNode;
+    className?: string;
+}) => {
+    return (
+        <section className="shadow-dark bg-[#FAFAFA]">
+            <div className={`container mx-auto py-20 ${className}`}>
+                {children}
+            </div>
+        </section>
+    );
+};
+
+export default GreySection;
---
+++ src/common/WhiteSection.tsx
@@ -0,0 +1,19 @@
+import type { ReactNode } from 'react';
+
+const WhiteSection = ({
+    children,
+    className
+}: {
+    children: ReactNode;
+    className?: string;
+}) => {
+    return (
+        <section className="border-b-solid border-b-1 border-b-[#dddddd] bg-[#FFFFFF]">
+            <div className={`container mx-auto py-20 ${className}`}>
+                {children}
+            </div>
+        </section>
+    );
+};
+
+export default WhiteSection;
