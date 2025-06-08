9ee0e428afdc598930c3467fc9e17a7598c7ae59
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-06-08 11:56:53 -0400

    Add toast container

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import Header from '../common/Header';
 import Footer from '../common/Footer';
 
 const RootLayout = () => {
     return (
         <>
+            <ToastContainer position="bottom-right" />
             <Header />
             <main className="overflow-hidden">
                 <Outlet />
