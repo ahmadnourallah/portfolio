5864be77957204f7cb27824ac02a4322b5602ede
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-06-08 08:12:20 -0400

    Create button component

---
+++ src/common/ActionButton.tsx
@@ -0,0 +1,22 @@
+import type { MouseEventHandler, ReactNode } from 'react';
+
+const ActionButton = ({
+    children,
+    className,
+    onClick
+}: {
+    children: ReactNode;
+    className?: string;
+    onClick?: MouseEventHandler;
+}) => {
+    return (
+        <button
+            className={`font-proxima rounded-lg bg-[#333333] px-10 py-3 text-xl font-bold text-white transition-opacity duration-200 hover:opacity-80 ${className}`}
+            onClick={onClick}
+        >
+            {children}
+        </button>
+    );
+};
+
+export default ActionButton;
