f8e0e5e985f339c907d2d1c9ba05911301ee1c69
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-06-09 13:53:41 -0400

    Add infinite scrolling

--- src/components/pages/BlogPage.tsx
+++ src/components/pages/BlogPage.tsx
@@ -1,22 +1,41 @@
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import { toast } from 'react-toastify';
+import { Fragment } from 'react/jsx-runtime';
 import WhiteSection from '../common/WhiteSection';
 import BlogArticleCard from '../blog/BlogArticleCard';
 import ReactMarkdown from 'react-markdown';
 import Spinner from '../common/Spinner';
 import LoadingError from '../common/LoadingError';
+import ActionButton from '../common/ActionButton';
 
 const BlogPage = () => {
-    const { isPending, isSuccess, refetch, isError, data, error } = useQuery({
+    const {
+        isPending,
+        isFetchingNextPage,
+        isSuccess,
+        refetch,
+        isError,
+        data,
+        fetchNextPage,
+        hasNextPage,
+        error
+    } = useInfiniteQuery({
         queryKey: ['posts'],
-        queryFn: async () => {
-            const response = await fetch(`${import.meta.env.VITE_API}/posts`);
+        queryFn: async ({ pageParam }) => {
+            const response = await fetch(
+                `${import.meta.env.VITE_API}/posts?start=${pageParam + 1}&end=${pageParam + 10}&orderby=date&order=desc`
+            );
 
             if (!response.ok) {
                 throw new Error("Server isn't responding!");
             }
 
             return response.json();
+        },
+        initialPageParam: 0,
+        getNextPageParam: (lastPage, pages, lastPageParam) => {
+            if (lastPage.data.count < 10) return undefined;
+            return lastPageParam + 10;
         }
     });
 
@@ -41,19 +60,43 @@ const BlogPage = () => {
 
             {isSuccess && (
                 <WhiteSection className="flex flex-col gap-25 sm:px-40">
-                    {data.data.posts.map((post) => (
-                        <BlogArticleCard
-                            id={post.id}
-                            title={post.title}
-                            summary={
-                                <ReactMarkdown
-                                    children={`${post.content.slice(0, 150)}...`}
+                    {data.pages.map((page, i) => (
+                        <Fragment key={i}>
+                            {page.data.posts.map((post) => (
+                                <BlogArticleCard
+                                    id={post.id}
+                                    title={post.title}
+                                    summary={
+                                        <ReactMarkdown
+                                            children={`${post.content.slice(0, 150)}...`}
+                                        />
+                                    }
+                                    thumbnail={post.thumbnail}
+                                    date={post.createdAt}
                                 />
-                            }
-                            thumbnail={post.thumbnail}
-                            date={post.createdAt}
-                        />
+                            ))}
+                        </Fragment>
                     ))}
+
+                    <ActionButton
+                        disabled={isPending || !hasNextPage}
+                        className="flex justify-center px-0 disabled:opacity-80"
+                        onClick={
+                            hasNextPage ? () => fetchNextPage() : undefined
+                        }
+                    >
+                        {isFetchingNextPage ? (
+                            <Spinner
+                                bg="grey"
+                                spinnerColor="white"
+                                size="28px"
+                            />
+                        ) : hasNextPage ? (
+                            'Load More'
+                        ) : (
+                            'Nothing more to load'
+                        )}
+                    </ActionButton>
                 </WhiteSection>
             )}
         </>
