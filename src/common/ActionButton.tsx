6e41cfbf9adb14ef697075f3abba4a84a2d31b31
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-06-09 11:35:35 -0400

    Customize action button

--- src/common/ActionButton.tsx
+++ src/common/ActionButton.tsx
@@ -3,14 +3,17 @@ import type { MouseEventHandler, ReactNode } from 'react';
 const ActionButton = ({
     children,
     className,
-    onClick
+    onClick,
+    disabled = false
 }: {
     children: ReactNode;
     className?: string;
     onClick?: MouseEventHandler;
+    disabled?: boolean;
 }) => {
     return (
         <button
+            disabled={disabled}
             className={`font-proxima rounded-lg bg-[#317F39] px-10 py-3 text-xl font-bold text-white transition-opacity duration-200 hover:opacity-80 ${className}`}
             onClick={onClick}
         >
