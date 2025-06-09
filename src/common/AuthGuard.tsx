c137822db5e4b113d43e261e6f00a331cd107188
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-06-09 11:41:03 -0400

    Create route protection components

---
+++ src/common/AuthGuard.tsx
@@ -0,0 +1,17 @@
+import type { ReactNode } from 'react';
+import { toast } from 'react-toastify';
+import { Navigate } from 'react-router-dom';
+import useLoggingStatus from '../hooks/useLoggingStatus';
+
+const AuthGuard = ({ component }: { component: ReactNode }) => {
+    const isLoggedIn = useLoggingStatus();
+
+    if (!isLoggedIn) {
+        toast.error("You're not logged in!");
+        return <Navigate to="/login" />;
+    }
+
+    return <>{component}</>;
+};
+
+export default AuthGuard;
---
+++ src/common/UnAuthGuard.tsx
@@ -0,0 +1,17 @@
+import { type ReactNode } from 'react';
+import { Navigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import useLoggingStatus from '../hooks/useLoggingStatus';
+
+const UnAuthGuard = ({ component }: { component: ReactNode }) => {
+    const isLoggedIn = useLoggingStatus();
+
+    if (isLoggedIn) {
+        toast.warning("You're already logged in!");
+        return <Navigate to="/" />;
+    }
+
+    return <>{component}</>;
+};
+
+export default UnAuthGuard;
