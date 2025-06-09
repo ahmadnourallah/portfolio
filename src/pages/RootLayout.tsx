d7749ab68682bdeac088e742c548dea964476ddb
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-06-09 11:39:10 -0400

    Remove toast container from layout

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
 import Header from '../common/Header';
 import Footer from '../common/Footer';
 
 const RootLayout = () => {
     return (
         <>
-            <ToastContainer position="bottom-right" />
             <Header />
             <main className="overflow-hidden">
                 <Outlet />
