fa468b9dc163f109afe5f637253460c8c2e395e1
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-06-10 08:56:59 -0400

    Use right type

--- src/components/common/LoadingError.tsx
+++ src/components/common/LoadingError.tsx
@@ -1,6 +1,7 @@
 import ActionButton from './ActionButton';
+import type { MouseEventHandler } from 'react';
 
-const LoadingError = ({ onClick }: { onClick: CallableFunction }) => {
+const LoadingError = ({ onClick }: { onClick: MouseEventHandler }) => {
     return (
         <div className="absolute top-1/2 left-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center gap-5">
             <p className="font-proxima text-center text-2xl font-bold">
