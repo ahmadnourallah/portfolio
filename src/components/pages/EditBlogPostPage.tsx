f04f4ca68aa9289a7839e4f9c6ac62c54fc1403d
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-06-12 07:38:26 -0400

    Create a post editing page

---
+++ src/components/pages/EditBlogPostPage.tsx
@@ -0,0 +1,62 @@
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { toast } from 'react-toastify';
+import { getPost } from '../../queries/post';
+import BlogPostEditor from '../blog/BlogPostEditor';
+import Spinner from '../common/Spinner';
+import LoadingError from '../common/LoadingError';
+import NotFound from '../common/NotFound';
+
+const EditBlogPostPage = () => {
+    const { postId } = useParams();
+
+    const {
+        isPending,
+        isSuccess,
+        isError,
+        refetch,
+        data: post,
+        error
+    } = useQuery({
+        queryKey: ['post', postId],
+        queryFn: async () => await getPost(postId as string)
+    });
+
+    if (isError) {
+        if (error.message === '404') return <NotFound />;
+        else {
+            toast.dismiss();
+            toast.error(error.message);
+        }
+    }
+
+    return (
+        <>
+            {isPending && (
+                <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 items-center justify-center">
+                    <Spinner size="100px" />
+                </div>
+            )}
+
+            {isError && (
+                <LoadingError
+                    onClick={() => {
+                        toast.dismiss();
+                        refetch();
+                    }}
+                />
+            )}
+
+            {isSuccess && (
+                <BlogPostEditor
+                    id={post.id}
+                    title={post.title}
+                    content={post.content}
+                    thumbnail={post.thumbnail}
+                />
+            )}
+        </>
+    );
+};
+
+export default EditBlogPostPage;
