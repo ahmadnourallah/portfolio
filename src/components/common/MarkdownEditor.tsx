d7983f63dc9fcc2c792bd0e71e2279d79d5784d1
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-06-12 07:35:32 -0400

    Create a markdown editor

---
+++ src/components/common/MarkdownEditor.tsx
@@ -0,0 +1,106 @@
+import {
+    headingsPlugin,
+    listsPlugin,
+    quotePlugin,
+    markdownShortcutPlugin,
+    MDXEditor,
+    toolbarPlugin,
+    UndoRedo,
+    codeMirrorPlugin,
+    BoldItalicUnderlineToggles,
+    CreateLink,
+    linkDialogPlugin,
+    CodeToggle,
+    Separator,
+    StrikeThroughSupSubToggles,
+    ListsToggle,
+    BlockTypeSelect,
+    InsertTable,
+    InsertCodeBlock,
+    codeBlockPlugin,
+    ChangeCodeMirrorLanguage,
+    ConditionalContents,
+    tablePlugin,
+    type MDXEditorMethods
+} from '@mdxeditor/editor';
+import '@mdxeditor/editor/style.css';
+import type { Ref } from 'react';
+
+const MarkdownEditor = ({
+    ref,
+    content = '',
+    disabled = false
+}: {
+    ref?: Ref<MDXEditorMethods>;
+    content?: string;
+    disabled?: boolean;
+}) => {
+    return (
+        <div className="h-100 overflow-y-auto rounded-[var(--radius-medium)] border-1 border-[#dddddd]">
+            <MDXEditor
+                readOnly={disabled}
+                ref={ref}
+                markdown={content}
+                className="prose max-w-none"
+                contentEditableClassName="h-full"
+                plugins={[
+                    linkDialogPlugin(),
+                    headingsPlugin({ allowedHeadingLevels: [2, 3] }),
+                    listsPlugin(),
+                    quotePlugin(),
+                    tablePlugin(),
+                    markdownShortcutPlugin(),
+                    codeBlockPlugin({ defaultCodeBlockLanguage: 'js' }),
+                    codeMirrorPlugin({
+                        codeBlockLanguages: {
+                            js: 'JavaScript',
+                            css: 'CSS',
+                            html: 'HTML'
+                        }
+                    }),
+                    toolbarPlugin({
+                        toolbarClassName: 'mdxToolbar',
+                        toolbarContents: () => (
+                            <>
+                                <UndoRedo />
+                                <Separator />
+                                <BoldItalicUnderlineToggles />
+                                <CodeToggle />
+                                <Separator />
+                                <StrikeThroughSupSubToggles />
+                                <Separator />
+                                <ListsToggle />
+                                <Separator />
+                                <BlockTypeSelect />
+                                <Separator />
+                                <CreateLink />
+                                <InsertTable />
+                                <ConditionalContents
+                                    options={[
+                                        {
+                                            when: (editor) =>
+                                                editor?.editorType ===
+                                                'codeblock',
+                                            contents: () => (
+                                                <ChangeCodeMirrorLanguage />
+                                            )
+                                        },
+                                        {
+                                            fallback: () => (
+                                                <>
+                                                    <InsertCodeBlock />
+                                                </>
+                                            )
+                                        }
+                                    ]}
+                                />
+                            </>
+                        )
+                    })
+                ]}
+            />
+        </div>
+    );
+};
+
+export default MarkdownEditor;
