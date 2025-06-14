533388ee608e4a96164731bfb0d4fdaf9178e10d
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-06-14 08:22:15 -0400

    Add delete post functionality

--- src/components/pages/BlogPostPage.tsx
+++ src/components/pages/BlogPostPage.tsx
@@ -1,109 +1,106 @@
-import { useParams, Link } from 'react-router-dom';
-import { useQuery } from '@tanstack/react-query';
+import { useParams, Link, useNavigate } from 'react-router-dom';
+import { useQuery, useMutation } from '@tanstack/react-query';
 import { toast } from 'react-toastify';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { a11yDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import type { ReactNode } from 'react';
-import { getPost } from '../../queries/post';
+import { useContext } from 'react';
+import { getPost, deletePost } from '../../queries/post';
+import { AuthContext } from '../../context/AuthContextProvider';
 import ReactMarkdown from 'react-markdown';
 import Spinner from '../common/Spinner';
 import BlogArticleCard from '../blog/BlogArticleCard';
 import WhiteSection from '../common/WhiteSection';
 import LoadingError from '../common/LoadingError';
 import NotFound from '../common/NotFound';
-
-const code = ({
-    children,
-    className,
-    ...rest
-}: {
-    children?: ReactNode;
-    className?: string;
-}) => {
-    const match = /language-(\w+)/.exec(className || '');
-    return match ? (
-        <SyntaxHighlighter
-            {...rest}
-            PreTag="div"
-            children={String(children).replace(/\n$/, '')}
-            language={match[1]}
-            showLineNumbers={true}
-            style={a11yDark}
-        />
-    ) : (
-        <code {...rest} className={className}>
-            {children}
-        </code>
-    );
-};
+import Code from '../blog/Code';
 
 const BlogPostPage = () => {
+    const navigate = useNavigate();
+    const { user } = useContext(AuthContext);
     const { postId } = useParams();
-    const {
-        isPending,
-        isSuccess,
-        isError,
-        refetch,
-        data: post,
-        error
-    } = useQuery({
+
+    const deletePostMutation = useMutation({
+        mutationFn: (id: number) => deletePost(id, user.token)
+    });
+
+    const { data: post, ...getPostMutation } = useQuery({
         queryKey: ['post', postId],
         queryFn: () => getPost(postId as string)
     });
 
-    if (isError) {
-        if (error.message === '404') return <NotFound />;
+    if (getPostMutation.isError) {
+        if (getPostMutation.error.message === '404') return <NotFound />;
         else {
             toast.dismiss();
-            toast.error(error.message);
+            toast.error(getPostMutation.error.message);
         }
+    }
+
+    if (deletePostMutation.isError) {
+        toast.dismiss();
+        toast.error(deletePostMutation.error?.message);
+    }
+
+    if (deletePostMutation.isSuccess) {
+        toast.dismiss();
+        toast.success('Post has been deleted successfully!');
+        navigate('/');
     }
 
     return (
         <>
-            {isPending && (
+            {(getPostMutation.isPending || deletePostMutation.isPending) && (
                 <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 items-center justify-center">
                     <Spinner size="100px" />
                 </div>
             )}
 
-            {isError && (
+            {getPostMutation.isError && (
                 <LoadingError
                     onClick={() => {
                         toast.dismiss();
-                        refetch();
+                        getPostMutation.refetch();
                     }}
                 />
             )}
 
-            {isSuccess && (
+            {getPostMutation.isSuccess && !deletePostMutation.isPending && (
                 <WhiteSection className="flex flex-col gap-10 sm:px-40">
-                    <title>{`Ahmad Nour Alla - ${post.title}`}</title>
+                    <title>{`Ahmad Nour Alla - ${post?.title}`}</title>
 
-                    <Link
-                        className="font-proxima cursor-pointer text-xl text-[#111111] underline hover:text-[#757575]"
-                        to={`/blog/${postId}/edit`}
-                    >
-                        Edit
-                    </Link>
+                    <div className="flex gap-5">
+                        <Link
+                            className="font-proxima cursor-pointer text-xl text-[#111111] underline hover:text-[#757575]"
+                            to={`/blog/${postId}/edit`}
+                        >
+                            Edit
+                        </Link>
+
+                        <a
+                            onClick={() => {
+                                deletePostMutation.mutate(Number(postId));
+                            }}
+                            className="font-proxima cursor-pointer text-xl text-[#111111] underline hover:text-[#757575]"
+                        >
+                            Delete
+                        </a>
+                    </div>
 
                     <BlogArticleCard
-                        id={post.id}
-                        title={post.title}
+                        id={post?.id as number}
+                        title={post?.title as string}
                         summary={
                             <ReactMarkdown
-                                children={`${post.content.slice(0, 150)}...`}
+                                children={`${post?.content.slice(0, 150)}...`}
                             />
                         }
-                        thumbnail={post.thumbnail}
-                        date={post.createdAt}
+                        thumbnail={post?.thumbnail as string}
+                        date={post?.createdAt as string}
                         topImage={false}
                     />
 
                     <article className="blog">
                         <ReactMarkdown
-                            children={post.content}
-                            components={{ code }}
+                            children={post?.content}
+                            components={{ code: Code }}
                         />
                     </article>
                 </WhiteSection>
