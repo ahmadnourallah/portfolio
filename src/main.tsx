f9b599dd9dc6283726a417a079584f62251e6f66
Ahmad Nourallah <ahmadnurallah@gmail.com>
2026-01-12 13:08:29 -0500

    Use Hash Router to support static hosting

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AuthContextProvider } from './context/AuthContextProvider';
 import { ToastContainer } from 'react-toastify';
@@ -27,7 +27,7 @@ createRoot(document.getElementById('root')!).render(
         <ToastContainer limit={1} position="bottom-right" />
         <AuthContextProvider>
             <QueryClientProvider client={queryClient}>
-                <BrowserRouter>
+                <HashRouter>
                     <Routes>
                         <Route element={<RootLayout />}>
                             <Route path="*" element={<NotFound />} />
@@ -58,7 +58,7 @@ createRoot(document.getElementById('root')!).render(
 
                         <Route path="/logout" element={<LogoutPage />} />
                     </Routes>
-                </BrowserRouter>
+                </HashRouter>
             </QueryClientProvider>
         </AuthContextProvider>
     </StrictMode>
