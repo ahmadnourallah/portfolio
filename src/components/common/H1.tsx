6acae8ab71b786af3ace5b196c096bba5621d1a0
Ahmad Nourallah <ahmadnurallah@gmail.com>
2026-01-13 07:06:55 -0500

    Change styles

--- src/components/common/H1.tsx
+++ src/components/common/H1.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from 'react';
 
 const H1 = ({ children }: { children: ReactNode }) => {
     return (
-        <h1 className="font-paroxima text-8xl font-bold -tracking-widest text-[#333333]">
+        <h1 className="font-paroxima text-7xl font-bold -tracking-widest text-[#333333] sm:text-8xl">
             {children}
         </h1>
     );
